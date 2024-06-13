eef33cef5a725d260587e0a4cb57b061a8d71ec1
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-06-13 18:39:10 -0300

    Transformei o logo da câmara em um hyperlink para facilitar o retorno à pagina Access

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,17 @@
 import styles from "./Header.module.css";
 import LogoCamara from "../../assets/CamaraSemFundoBranco.png";
 
+import { useNavigate } from "react-router-dom";
+
 export const Header = () => {
+  const navigate = useNavigate();
+  
   return (
     <div className={styles.header}>
       <img
         src={LogoCamara}
         alt="Logo Câmara de Vereadores de Balneário Camboriú"
+        onClick={() => navigate("/")}
       />
       <h1>Admissão Digital</h1>
     </div>
