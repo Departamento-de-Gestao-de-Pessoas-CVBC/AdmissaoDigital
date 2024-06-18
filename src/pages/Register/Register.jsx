554962e4cadc70a182637e14dfda8d8a17721b1e
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-06-18 15:09:34 -0300

    Resolvido o bug de não retornar a página de Acesso após a finalização do cadastro.

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,28 +1,17 @@
 import styles from "./Register.module.css";
-import { Routes, Route } from "react-router-dom";
 
-import React, { useState } from "react";
-import { GoArrowRight, GoArrowLeft } from "react-icons/go";
+import React from "react";
 
 import { Header } from "../../components/Header/Header";
-import { Button } from "../../components/Button/Button";
-import { PersonalData } from "../../components/MultiStepForm/PersonalData";
 import { MultiStepForm } from "../../components/MultiStepForm/MultiStepForm";
 
 export const Register = () => {
   return (
     <div className={styles.register}>
       <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className={styles.multiStepForm}>
-              <MultiStepForm />
-            </div>
-          }
-        />
-      </Routes>
+      <div className={styles.multiStepForm}>
+        <MultiStepForm />
+      </div>
     </div>
   );
 };
