796c2df1d11efb0edf2ffeb6c7f647932cc78240
Luiz Henrique <luiz.heenrike7@gmail.com>
2024-06-24 17:28:46 -0300

    Construída a página de Acesso Administrativo.

--- src/pages/AdminAccess/AdminAccess.jsx
+++ src/pages/AdminAccess/AdminAccess.jsx
@@ -1,9 +1,43 @@
 import styles from "./AdminAccess.module.css";
+import LogoCamara from "../../assets/CamaraSemFundoAzul.png";
+
+import { useNavigate } from "react-router-dom";
+
+import LoginIcon from "@mui/icons-material/Login";
+import LogoutIcon from "@mui/icons-material/Logout";
+
+import { Input } from "../../components/Input/Input";
+import { BasicButton } from "../../components/BasicButton/BasicButton";
 
 export const AdminAccess = () => {
+  const navigate = useNavigate();
+
   return (
-    <div>
-      <div></div>
+    <div className={styles.container}>
+      <div className={styles.loginCard}>
+        <div className={styles.imgTitle}>
+          <img
+            src={LogoCamara}
+            alt="Logo Câmara de Vereadores de Balneário Camboriú"
+          />
+          <h1>Acesso Administrador</h1>
+        </div>
+        <div className={styles.inputs}>
+          <Input
+            type="number"
+            id="login"
+            label="Login (CPF)"
+            mask="999.999.999-99"
+          />
+          <Input type="password" id="password" label="Senha" />
+          <BasicButton title="Entrar" startIcon={<LoginIcon />} />
+          <BasicButton
+            title="Sair"
+            startIcon={<LogoutIcon />}
+            onClick={() => navigate("/")}
+          />
+        </div>
+      </div>
     </div>
   );
 };
