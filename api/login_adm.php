b6b30bb5f3096ae987508d773e1e51a7774b9db9
gusmoreira02 <gusmoreira02@gmail.com>
2024-07-12 18:25:02 -0300

    Teste com banco de dados online

--- api/login_adm.php
+++ api/login_adm.php
@@ -3,9 +3,19 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
-$conn = new mysqli("localhost", "root", "", "dgp");
-if (mysqli_connect_error()) {
-    echo mysqli_connect_error();
+// Detalhes da conexão Reilwai
+$servername = "viaduct.proxy.rlwy.net"; // Endereço do servidor Reilwai
+$username = "root"; // Nome de usuário Reilwai
+$password = "SNmpcPoRunXApXaQEuosXNaOYqGOAfyc"; // Senha Reilwai
+$dbname = "railway"; // Nome do banco de dados Reilwai
+$port = 58726; // Porta Reilwai
+
+// Cria a conexão
+$conn = new mysqli($servername, $username, $password, $dbname, $port);
+
+// Verifica a conexão
+if ($conn->connect_error) {
+    echo json_encode(["message" => "Connection failed: " . $conn->connect_error]);
     exit();
 } else {
     $eData = file_get_contents("php://input");
@@ -18,22 +28,26 @@ if (mysqli_connect_error()) {
         $sql = "SELECT * FROM user_adm WHERE usuario='$user';";
         $res = mysqli_query($conn, $sql);
 
-        if (mysqli_num_rows($res) != 0) {
-            $row = mysqli_fetch_array($res);
-            $hashed_password_from_db = $row['senha'];
-            $token = $row['token'];
+        if ($res) {
+            if (mysqli_num_rows($res) != 0) {
+                $row = mysqli_fetch_array($res);
+                $hashed_password_from_db = $row['senha'];
+                $token = $row['token'];
 
-            if (password_verify($pass, $hashed_password_from_db)) {
-                $result = array(
-                    "message" => "Logado com sucesso! Redirecionando...",
-                    "userId" => $row['id'],
-                    "token" => $token
-                );
+                if (password_verify($pass, $hashed_password_from_db)) {
+                    $result = array(
+                        "message" => "Logado com sucesso! Redirecionando...",
+                        "userId" => $row['id'],
+                        "token" => $token
+                    );
+                } else {
+                    $result = array("message" => "Senha incorreta!");
+                }
             } else {
-                $result = array("message" => "Senha incorreta!");
+                $result = array("message" => "Usuário incorreto!");
             }
         } else {
-            $result = array("message" => "Usuário incorreto!");
+            $result = array("message" => "Erro na consulta ao banco de dados: " . mysqli_error($conn));
         }
     } else {
         $result = array("message" => "Preencha todos os campos!");
