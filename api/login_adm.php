a99b5f5dbc0bba8d8d7c21e40b78f17c49760697
gusmoreira02 <gusmoreira02@gmail.com>
2024-07-12 18:31:09 -0300

    voltando ao normal

--- api/login_adm.php
+++ api/login_adm.php
@@ -3,57 +3,43 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Headers: *");
 header("Access-Control-Allow-Methods: *");
 
-// Detalhes da conexão Reilwai
-$servername = "viaduct.proxy.rlwy.net";
-$username = "root";
-$password = "SNmpcPoRunXApXaQEuosXNaOYqGOAfyc";
-$dbname = "railway";
-$port = 58726; // Porta do banco de dados
-
-// Cria a conexão
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
-// Verifica a conexão
-if ($conn->connect_error) {
-    echo json_encode(["message" => "Connection failed: " . $conn->connect_error]);
+$conn = new mysqli("localhost", "root", "", "dgp");
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
     exit();
-} 
+} else {
+    $eData = file_get_contents("php://input");
+    $dData = json_decode($eData, true);
 
-// Recebe os dados da requisição
-$eData = file_get_contents("php://input");
-$dData = json_decode($eData, true);
+    $user = $dData['user'];
+    $pass = $dData['pass'];
 
-$user = $dData['user'];
-$pass = $dData['pass'];
+    if ($user != "" && $pass != "") {
+        $sql = "SELECT * FROM user_adm WHERE usuario='$user';";
+        $res = mysqli_query($conn, $sql);
 
-if ($user != "" && $pass != "") {
-    $sql = "SELECT * FROM user_adm WHERE usuario=?";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("s", $user);
-    $stmt->execute();
-    $res = $stmt->get_result();
+        if (mysqli_num_rows($res) != 0) {
+            $row = mysqli_fetch_array($res);
+            $hashed_password_from_db = $row['senha'];
+            $token = $row['token'];
 
-    if ($res->num_rows != 0) {
-        $row = $res->fetch_assoc();
-        $hashed_password_from_db = $row['senha'];
-        $token = $row['token'];
-
-        if (password_verify($pass, $hashed_password_from_db)) {
-            $result = array(
-                "message" => "Logado com sucesso! Redirecionando...",
-                "userId" => $row['id'],
-                "token" => $token
-            );
+            if (password_verify($pass, $hashed_password_from_db)) {
+                $result = array(
+                    "message" => "Logado com sucesso! Redirecionando...",
+                    "userId" => $row['id'],
+                    "token" => $token
+                );
+            } else {
+                $result = array("message" => "Senha incorreta!");
+            }
         } else {
-            $result = array("message" => "Senha incorreta!");
+            $result = array("message" => "Usuário incorreto!");
         }
     } else {
-        $result = array("message" => "Usuário incorreto!");
+        $result = array("message" => "Preencha todos os campos!");
     }
-} else {
-    $result = array("message" => "Preencha todos os campos!");
-}
 
-$conn->close();
-echo json_encode($result);
+    $conn->close();
+    echo json_encode($result);
+}
 ?>
