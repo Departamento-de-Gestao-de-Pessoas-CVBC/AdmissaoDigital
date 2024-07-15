a0933a52eb9df1ab4e4c4a97cf51e7a8707a16c9
gusmoreira02 <gusmoreira02@gmail.com>
2024-07-15 17:07:28 -0300

    teste

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,
+    port: 5173 // ou qualquer outra porta que você esteja usando
+  }
 });
